8b63eaa666afe2d4b2620c4f23ca6985f325cd23
sascott9655 <samscott027@gmail.com>
2025-05-16 13:31:25 -0700

    deleted css files that were not used, also hid the moderate button that only admin has access too!

--- static/js/index.js
+++ static/js/index.js
@@ -11,29 +11,3 @@ document.addEventListener('DOMContentLoaded', function(){
         button.addEventListener('click', deleteJoke);
     });
 });
-
-// function toggleComments(id, button){
-//     const commentsDiv = document.getElementById(id);
-//     if(commentsDiv.style.display === "none") {
-//         commentsDiv.style.display = "block";
-//         button.textContent = "Hide Comments";
-//     } else {
-//         commentsDiv.style.display = "none";
-//         button.textContent = "Show Comments";
-//     }
-// }
-
-// document.addEventListener('DOMContentLoaded', function(){
-//     const commentButton = document.querySelectorAll('.toggle-comments');
-//     commentButton.forEach(function(button){
-//         const jokeId = button.getAttribute('data-joke-id');
-//         const commentDivId = `comments-${jokeId}`;
-
-//         // Set initial text
-//         button.textContent = "Show Comments";
-
-//         button.addEventListener('click', function(){
-//             toggleComments(commentDivId, button);
-//         });
-//     })
-// });
